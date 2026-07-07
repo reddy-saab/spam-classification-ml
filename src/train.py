f038845d61e797a437f08f9d4630aa22c292100e
reddy-saab <kattaanish14376@gmail.com>
2026-07-08 00:41:27 +0530

    Model trained and evaluated

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ import preprocess
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,precision_score, recall_score,f1_score
 
 df=pd.read_csv('../Data/email.csv')
 #print(df.head())
@@ -26,9 +27,19 @@ df['transformed_text']=df['Message'].apply(preprocess.transform_text)
 #vectorization
 tfidf=TfidfVectorizer()
 X=tfidf.fit_transform(df['transformed_text']).toarray()
+y=df['Category'].values
 
-print(X)
 
+x_train, x_test, y_train, y_test= train_test_split(X,y,train_size=0.8,random_state=30)
+LR= LogisticRegression()
+
+LR.fit(x_train,y_train)
+y_pred=LR.predict(x_test)
+
+print(f"Accuracy: {accuracy_score(y_test,y_pred):.2f}") 
+print(f"Precision: {precision_score(y_test,y_pred):.2f}")
+print(f"Recall: {recall_score(y_test,y_pred):.2f}")
+print(f"F1: {f1_score(y_test,y_pred):.2f}")
 
 
 
