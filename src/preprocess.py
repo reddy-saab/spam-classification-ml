0038cf0b02bbd9c5f18df8e3a3ad721989c6bb05
reddy-saab <kattaanish14376@gmail.com>
2026-07-07 18:03:11 +0530

    Text preprocessing function added

---
+++ src/preprocess.py
@@ -0,0 +1,34 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+#nltk.download('stopwords')
+
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+
+def transform_text(text):
+  """
+  Cleans an email by:
+  - converting to lowercase
+  - removing stopwords
+  - removing punctuation
+  - applying stemming
+  """
+  text= text.lower()
+  text= nltk.word_tokenize(text)
+  y=[]
+  for i in text:
+    if(i.isalnum()):
+      y.append(i)
+  text=y[:]
+  y.clear()
+  for i in text:
+    if(i not in string.punctuation and i not in stopwords.words('english')):
+      y.append(ps.stem(i))
+  return " ".join(y)
+
+
+
+if __name__=='__main__':
+  print(transform_text("How are you singing anil?"))
