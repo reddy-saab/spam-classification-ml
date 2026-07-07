c3b9c71de0d31dd868c99754a914069c7f406223
reddy-saab <kattaanish14376@gmail.com>
2026-07-07 23:51:26 +0530

    vectorization done

--- src/train.py
+++ src/train.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import preprocess
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 df=pd.read_csv('../Data/email.csv')
 #print(df.head())
@@ -12,10 +16,20 @@ df['Category']=encoder.fit_transform(df['Category'])
 #print(df.head())
 
 #check duplicates
-print(df.duplicated().sum()) # 415 values
+#print(df.duplicated().sum()) # 415 values
 
 df.drop_duplicates(keep='first',inplace=True)
-print(df.duplicated().sum())
+#print(df.duplicated().sum())
+
+df['transformed_text']=df['Message'].apply(preprocess.transform_text)
+
+#vectorization
+tfidf=TfidfVectorizer()
+X=tfidf.fit_transform(df['transformed_text']).toarray()
+
+print(X)
+
+
 
 
 
